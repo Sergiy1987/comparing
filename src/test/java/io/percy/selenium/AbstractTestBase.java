f970316c9486a520d4a250f3ef7425245b9ce99b
Sergiy1987 <nedved198725@gmail.com>
2022-11-29 13:56:36 +0200

    add files

--- src/test/java/io/percy/selenium/AbstractTestBase.java
+++ src/test/java/io/percy/selenium/AbstractTestBase.java
@@ -1,41 +1,70 @@
 package io.percy.selenium;
 
+import com.codeborne.selenide.WebDriverRunner;
+import io.percy.selenium.core.properties.Properties;
+import io.percy.selenium.core.properties.PropertiesLoader;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.TestInfo;
+import org.openqa.selenium.MutableCapabilities;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-
 public abstract class AbstractTestBase {
-    public static final String buildName = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+    protected static RemoteWebDriver driver;
 
-  /*  @Rule
-    public TestName testName = new TestName() {
-        public String getMethodName() {
-            return String.format("%s", super.getMethodName());
-        }
-    };*/
+    private static final String buildName = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
 
-//    @Rule
-//    public SauceTestWatcher resultReportingTestWatcher = new SauceTestWatcher();
+    public static void setUpDriver(String browserName, String platform,
+                                   String platformVersion, TestInfo testInfo,
+                                   String browserVersion, String screenResolution) throws MalformedURLException {
+        PropertiesLoader.loadProperties();
 
-    public static final String USERNAME = "nedved1";
-    public static final String AUTOMATE_KEY = "oTA4GGrEMQSwSLCxQM78";
-    public static final String URL = "https://" + USERNAME + ":" + AUTOMATE_KEY + "@hub-cloud.browserstack.com/wd/hub";
-/*    protected RemoteWebDriver driver;
+        MutableCapabilities browserOptions = setBrowserOptions(browserName, browserVersion);
+        MutableCapabilities browserstackOptions = setBrowserStackOptions(platform, platformVersion, testInfo, screenResolution);
+        browserOptions.setCapability("bstack:options", browserstackOptions);
 
-    public class SauceTestWatcher extends TestWatcher {
-        @Override
-        protected void failed(Throwable e, Description description) {
-            driver.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"failed\", \"reason\": " + "\""+e.getMessage() +"\"" +"}}");
-        }
+        String hubUrl = System.getProperty(Properties.BROWSER_STACK_USER_NAME) + ":" +
+                System.getProperty(Properties.BROWSER_STACK_API_KEY) + System.getProperty(Properties.BROWSER_STACK_HUB_URL);
 
-        @Override
-        protected void succeeded(Description description) {
-            driver.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \"passed\", \"reason\": \"Success\"}}");
-        }
+        driver = new RemoteWebDriver(new URL(hubUrl), browserOptions);
+        WebDriverRunner.setWebDriver(driver);
+    }
 
-        @Override
-        protected void finished(Description description) {
-            driver.quit();
-        }
-    }*/
+
+    private static MutableCapabilities setBrowserStackOptions(String platform, String platformVersion,
+                                                             TestInfo testInfo, String screenResolution) {
+        MutableCapabilities browserstackOptions = new MutableCapabilities();
+        browserstackOptions.setCapability("os", platform);
+        browserstackOptions.setCapability("osVersion", platformVersion);
+        browserstackOptions.setCapability("projectName", testInfo.getDisplayName());
+        browserstackOptions.setCapability("buildName", buildName);
+        browserstackOptions.setCapability("sessionName", "");
+        browserstackOptions.setCapability("resolution", screenResolution);
+        browserstackOptions.setCapability("local", "false");
+        browserstackOptions.setCapability("debug", "true");
+        browserstackOptions.setCapability("consoleLogs", "info");
+        browserstackOptions.setCapability("networkLogs", "true");
+        browserstackOptions.setCapability("video", "false");
+        browserstackOptions.setCapability("seleniumVersion", "4.1.0");
+        browserstackOptions.setCapability("telemetryLogs", "true");
+        return browserstackOptions;
+    }
+
+    private static MutableCapabilities setBrowserOptions(String browserName, String browserVersion) {
+        MutableCapabilities browserOptions = new MutableCapabilities();
+        browserOptions.setCapability(CapabilityType.BROWSER_NAME, browserName);
+        browserOptions.setCapability(CapabilityType.BROWSER_VERSION, browserVersion);
+        return browserOptions;
+    }
+
+    @AfterAll
+    public static void closeDriver() {
+        WebDriverRunner.getWebDriver().quit();
+    }
+
 }
